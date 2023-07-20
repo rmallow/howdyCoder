61cf4af5265123bd5d4148374200e0350e152e4a
rmallow <rmallow@crimson.ua.edu>
2023-07-20 12:07:08 -0400

    changing main to only set forkerver if not windows

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import multiprocessing as mp
+import platform
 
 if __name__ == "__main__":
-    # mp.freeze_support()
-    mp.set_start_method("forkserver")
+    if platform.system() != "Windows":
+        mp.set_start_method("forkserver")
     from algoBuilder.start import start
 
     start()
