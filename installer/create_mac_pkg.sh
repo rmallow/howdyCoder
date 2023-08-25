f903ab7f0bd258ddccee796d94e512e9eda6ee26
rmallow <rmallow@crimson.ua.edu>
2023-08-24 19:03:31 -0500

    adding -y for overwriting to installer command

--- installer/create_mac_pkg.sh
+++ installer/create_mac_pkg.sh
@@ -1,5 +1,5 @@
 python -m pyclean ../
-/usr/local/bin/platypus -B -R -i '../howdyCoder/ui/res/MyIcon.icns'  -a 'HowdyCoder'  -o 'None'  -p '/bin/sh'   -I com.rmallow.HowdyCoder -f '../main.py' -f '../requirements.txt' -f '../howdyCoder'  'start_mac.sh'
+/usr/local/bin/platypus -B -R -y -i '../howdyCoder/ui/res/MyIcon.icns'  -a 'HowdyCoder'  -o 'None'  -p '/bin/sh'   -I com.rmallow.HowdyCoder -f '../main.py' -f '../requirements.txt' -f '../howdyCoder'  'start_mac.sh'
 codesign --deep -s "Developer ID Application" -o runtime HowdyCoder.app
 ditto -c -k --keepParent HowdyCoder.app HowdyCoder.zip
 xcrun notarytool submit HowdyCoder.zip --wait -k ../../../Apple_Connect/AuthKey_CL4R868UJB.p8 -d CL4R868UJB -i 9764afda-07eb-4902-a15a-adafbea5c5bd
